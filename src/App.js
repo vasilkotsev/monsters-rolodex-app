87329409736c6bb60a9bee57038e05a0bd8a581d
Vasil Kotsev <vasil.kotsev@gmail.com>
2020-12-21 21:17:30 +0200

    adding app title

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1 className="app-title">Monsters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={this.handleChange}
